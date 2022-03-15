3454a377f1ca2914e0276d62b197d727bbcc502c
Kethiri <mohamed.kethiri@epita.com>
2022-03-15 12:25:15 +0100

    Add test to match 100% coverage

---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,10 @@
+package fr.lernejo.navy_battle;
+
+public class Launcher {
+
+    public static void main (String [] args)
+    {
+        System.out.print("Bonjour, Mohamed Rami KETHIRI est vraiment trop vivant !");
+    }
+
+}
